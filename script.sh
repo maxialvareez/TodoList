22f7a7232e14e159f2422d9d29b1b0bd03db4fa4
maxialvareez <maalvarez@alumnos.exa.unicen.edu.ar>
2022-02-21 18:19:24 -0300

    Add Script V2

---
+++ script.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/maxialvareez/TodoList.git
+cp .env.example .env
+php artisan key:generate
+composer install --no-dev --optimize-autoloader
+php artisan optimize
+php artisan route:cache
+php artisan cache:clear
+php artisan migrate
