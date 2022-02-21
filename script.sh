379f7e0694d451c57de22fe34f8d5662d22c08fe
maxialvareez <maalvarez@alumnos.exa.unicen.edu.ar>
2022-02-21 19:13:37 -0300

    Compilado y NPM

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@ git pull https://github.com/maxialvareez/TodoList.git
 cp .env.example .env
 php artisan key:generate
 composer install --no-dev --optimize-autoloader
+npm install && npm run dev
 php artisan optimize
 php artisan route:cache
 php artisan cache:clear
